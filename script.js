e2e13817b0891f13f2af40b296d2637b670ee99f
annamarcomini <annajuliamarcomini23@gmail.com>
2023-02-23 22:04:27 -0300

    :tada: initial commit

---
+++ script.js
@@ -0,0 +1,19 @@
+function toggleMode() {
+  const html = document.documentElement
+
+  html.classList.toggle("light")
+
+  const img = document.querySelector("#profile img")
+
+  if (html.classList.contains("light")) {
+    img.setAttribute("src", "Mobile/AvatarLight.png")
+  } else {
+    img.setAttribute("src", "Mobile/Avatar.png")
+  }
+
+  /*if(html.classList.contains('light')) {
+  html.classList.remove('light')
+ } else{
+  html.classList.add('light')
+ }*/
+}
